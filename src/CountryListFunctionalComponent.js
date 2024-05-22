ad6aa51ded73c0d10943a517796fb34abc7ff83b
Arun Mohandas <arun.mohandas@highpeaksw.com>
2024-05-22 16:19:53 +0530

    useState

--- src/CountryListFunctionalComponent.js
+++ src/CountryListFunctionalComponent.js
@@ -2,10 +2,14 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 export default function CountryListFunctionalComponent() {
+    const [countryList, setCountryList] = useState(['in', 'US', 'RUS']);
     return (
         <div>
             <div> hi from class component CountryList </div>
             <div> country list</div>
+            {countryList.map(c => (
+                <div> {c} </div>
+            ))}
         </div>
     );
 }
