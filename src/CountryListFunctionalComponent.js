c735a87ff022a925c97703051491ea7bd19a6869
Arun Mohandas <arun.mohandas@highpeaksw.com>
2024-05-22 16:20:14 +0530

    useEffect

--- src/CountryListFunctionalComponent.js
+++ src/CountryListFunctionalComponent.js
@@ -3,6 +3,33 @@ import axios from 'axios';
 
 export default function CountryListFunctionalComponent() {
     const [countryList, setCountryList] = useState(['in', 'US', 'RUS']);
+
+    useEffect(() => {
+        console.count('in use effect');
+
+        setTimeout(() => {
+            axios
+                .get('https://api.first.org/data/v1/countries')
+
+                .then(response => {
+                    console.log('in sucees');
+
+                    const { data } = (response && response.data) || {};
+
+                    console.log('ONLY DATA', data);
+
+                    const countryList = Object.keys(data);
+
+                    console.log('countryList', countryList);
+
+                })
+                .catch(error => {
+                    console.log('in catch');
+                    console.log('error', error);
+                });
+        }, 3000);
+    }, []);
+
     return (
         <div>
             <div> hi from class component CountryList </div>
